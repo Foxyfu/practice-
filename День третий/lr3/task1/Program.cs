c90ba25880d3c3f4746de829f61700194602ad14
37533 <37533@FOXUFY>
2023-04-07 19:47:01 +0300

    создание

---
+++ День третий/lr3/task1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            Console.WriteLine(a);
+
+        }
+    }
+}
